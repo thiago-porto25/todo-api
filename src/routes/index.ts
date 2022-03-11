2d458f551661bf804cac266f0bd0c8944032bbc0
Thiago Porto <thiago.porto@d3.do>
2022-03-10 21:05:29 -0300

    feat: create routes

---
+++ src/routes/index.ts
@@ -0,0 +1,14 @@
+import { Router } from 'express';
+import { getTodos, addTodo, deleteTodo, updateTodo } from 'controllers/todos';
+
+const router: Router = Router();
+
+router.get('/todos', getTodos);
+
+router.post('/add-todo', addTodo);
+
+router.put('/update-todo/:id', updateTodo);
+
+router.delete('/delete-todo/:id', deleteTodo);
+
+export default router;
