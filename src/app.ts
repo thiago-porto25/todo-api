1cbb48544e514f03e69b75e1bb09bbe276821c35
Thiago Porto <thiago.porto@d3.do>
2022-03-10 21:17:47 -0300

    feat: add logic to app.ts

---
+++ src/app.ts
@@ -0,0 +1,24 @@
+import express, { Express } from 'express';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import todoRoutes from 'routes';
+
+const app: Express = express();
+
+const PORT: string | number = process.env.PORT || 4000;
+
+app.use(cors());
+app.use(todoRoutes);
+
+const uri: string = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@clustertodo.raz9g.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority`;
+
+mongoose
+  .connect(uri)
+  .then(() =>
+    app.listen(PORT, () =>
+      console.log(`Server running on http://localhost:${PORT}`)
+    )
+  )
+  .catch((err) => {
+    throw err;
+  });
